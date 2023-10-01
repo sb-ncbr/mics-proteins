17df4513a0dd26c94ff274e51ba546455a9ec45b
Jakub Orsula <jakub.orsula@kiwi.com>
2023-10-01 22:04:04 +0200

    adjusted orchestration

--- sync-to-protein-jo.sh
+++ sync-to-protein-jo.sh
@@ -1,5 +1,5 @@
 rsync -av /home/jakub/git/school/diplomka/proteins/out/artifacts/proteins_jar/proteins.jar protein-jo:/home/ubuntu/protein-search/08_rebuildSeqScanAlgs/proteins-1.0-jar-with-dependencies.jar
-
+rsync -av /home/jakub/git/school/diplomka/proteins/out/artifacts/proteins_jar/proteins.jar protein-jo:/home/ubuntu/protein-search/05_PPP-codes/jars/proteins-1.0.jar
 #mvn install:install-file -Dfile=/home/jakub/git/school/diplomka/proteins/src/main/resources/messif-3.0.0-DEVEL.jar -DgroupId=messif -DartifactId=messif -Dversion=3.0.0-DEVEL -Dpackaging=jar
 #mvn install:install-file -Dfile=/home/jakub/git/school/diplomka/proteins/src/main/resources/SF_v3-3.0.jar -DgroupId=sf -DartifactId=SF_v3 -Dversion=3.0 -Dpackaging=jar
 
@@ -13,4 +13,17 @@ mvn install:install-file -Dfile=/home/jakub/git/school/diplomka/proteins/externa
                          -DgroupId=sf \
                          -DartifactId=SF_v3 \
                          -Dversion=3.0 \
+                         -Dpackaging=jar \
+
+
+mvn install:install-file -Dfile=/home/jakub/git/school/diplomka/proteins/src/main/resources/simcloud-1.0.0-DEVEL.jar \
+                         -DgroupId=simcloud \
+                         -DartifactId=simcloud \
+                         -Dversion=1.0.0-DEVEL \
+                         -Dpackaging=jar \
+
+mvn install:install-file -Dfile=/home/jakub/git/school/diplomka/proteins/src/main/resources/simcloud-mapdb-1.0.0-DEVEL.jar \
+                         -DgroupId=simcloud \
+                         -DartifactId=simcloud-mapdb \
+                         -Dversion=1.0.0-DEVEL \
                          -Dpackaging=jar \
