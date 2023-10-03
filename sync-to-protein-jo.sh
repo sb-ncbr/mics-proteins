22d1aed66de28772f5542b45c54e6b0b0a1e221e
Jakub Orsula <jakub.orsula@kiwi.com>
2023-10-03 10:49:24 +0200

    ppp codes can be fully orchestrated

--- sync-to-protein-jo.sh
+++ sync-to-protein-jo.sh
@@ -1,29 +1,21 @@
+# sync jar to sequential scan stage
 rsync -av /home/jakub/git/school/diplomka/proteins/out/artifacts/proteins_jar/proteins.jar protein-jo:/home/ubuntu/protein-search/08_rebuildSeqScanAlgs/proteins-1.0-jar-with-dependencies.jar
+
+# sync rest of jars
+# todo this can be accomplished by baking in the jars here
+rsync -av jars/ protein-jo:/home/ubuntu/protein-search/05_PPP-codes/jars/
+
+# sync protein jar to pppcodes
+# this has to be done after so the jar is overwritten
 rsync -av /home/jakub/git/school/diplomka/proteins/out/artifacts/proteins_jar/proteins.jar protein-jo:/home/ubuntu/protein-search/05_PPP-codes/jars/proteins-1.0.jar
-#mvn install:install-file -Dfile=/home/jakub/git/school/diplomka/proteins/src/main/resources/messif-3.0.0-DEVEL.jar -DgroupId=messif -DartifactId=messif -Dversion=3.0.0-DEVEL -Dpackaging=jar
-#mvn install:install-file -Dfile=/home/jakub/git/school/diplomka/proteins/src/main/resources/SF_v3-3.0.jar -DgroupId=sf -DartifactId=SF_v3 -Dversion=3.0 -Dpackaging=jar
 
-mvn install:install-file -Dfile=/home/jakub/git/school/diplomka/proteins/external_dependencies/messif-3.0.0-DEVEL.jar \
-                         -DgroupId=messif \
-                         -DartifactId=messif \
-                         -Dversion=3.0.0-DEVEL \
-                         -Dpackaging=jar \
+# sync startup script
+rsync -av pppcodes.sh protein-jo:/home/ubuntu/protein-search/05_PPP-codes/
 
-mvn install:install-file -Dfile=/home/jakub/git/school/diplomka/proteins/external_dependencies/SF_v3-3.0.jar \
-                         -DgroupId=sf \
-                         -DartifactId=SF_v3 \
-                         -Dversion=3.0 \
-                         -Dpackaging=jar \
+# sync configuration files
+rsync -av pppcodes.cf protein-jo:/home/ubuntu/protein-search/05_PPP-codes/
+rsync -av manager-pppcodes.cf protein-jo:/home/ubuntu/protein-search/05_PPP-codes/
 
-
-mvn install:install-file -Dfile=/home/jakub/git/school/diplomka/proteins/src/main/resources/simcloud-1.0.0-DEVEL.jar \
-                         -DgroupId=simcloud \
-                         -DartifactId=simcloud \
-                         -Dversion=1.0.0-DEVEL \
-                         -Dpackaging=jar \
-
-mvn install:install-file -Dfile=/home/jakub/git/school/diplomka/proteins/src/main/resources/simcloud-mapdb-1.0.0-DEVEL.jar \
-                         -DgroupId=simcloud \
-                         -DartifactId=simcloud-mapdb \
-                         -Dversion=1.0.0-DEVEL \
-                         -Dpackaging=jar \
+# sync ini file
+# todo one source of truth
+rsync -av protein_search.ini protein-jo:/home/ubuntu/protein-search/05_PPP-codes/
